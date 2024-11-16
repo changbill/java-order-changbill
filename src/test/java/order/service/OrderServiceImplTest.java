0d7f9c13005dd70daa61fd90ee8a3ebed7096b19
changbill <changbill@naver.com>
2024-11-16 13:22:14 +0900

    test(menu): add menu exception test

--- src/test/java/order/service/OrderServiceImplTest.java
+++ src/test/java/order/service/OrderServiceImplTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static order.exception.ExceptionMessage.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
@@ -18,7 +20,18 @@ class OrderServiceImplTest {
     @ValueSource(strings = {"햄버거(5개), 샐러드(2개), 콜라(3개)",
             "피자(2개), 감자튀김(1개), 콜라(3개)",
             "오렌지 주스(1개),감자튀김(10개),햄버거(5개)"})
-    void registerOrderTest(String testInput) {
+    void 메뉴_이름_검증_테스트(String testInput) {
         assertDoesNotThrow(() -> orderService.registerOrder(testInput));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"주먹밥(5개), 샐러드(2개), 콜라(3개)",
+    "피자(10개),감자튀김(1개),콜라(개)"})
+    void 메뉴_이름_검증_예외테스트(String testInput) {
+        assertThatThrownBy(() -> orderService.registerOrder(testInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]: ")
+                .hasMessageContaining(WRONG_MENU_NAME.getMessage());
+    }
+
 }
